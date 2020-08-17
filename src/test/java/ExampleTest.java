d884e1337e8565812626e7cff07447d1366c444c
isandratskiy <i.sandrackii@gmail.com>
2020-08-18 01:28:10 +0300

    test

--- src/test/java/ExampleTest.java
+++ src/test/java/ExampleTest.java
@@ -5,7 +5,8 @@ import org.junit.jupiter.api.Test;
 import static com.codeborne.selenide.AuthenticationType.BASIC;
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Configuration.*;
-import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 import static org.apache.commons.lang3.RandomStringUtils.randomAlphabetic;
 import static org.openqa.selenium.By.tagName;
 
@@ -17,18 +18,6 @@ public class ExampleTest {
         baseUrl = "https://the-internet.herokuapp.com";
         browser = "chrome";
         remote = "http://127.0.0.1:4444/wd/hub";
-    }
-
-    @Test
-    void canEditIFrameText() {
-        var inputText = randomAlphabetic(200);
-        open("/");
-        $$(".columns ul > li a").find(matchText("WYSIWYG")).scrollTo().click();
-        switchTo().frame("mce_0_ifr");
-        var editor = $(".mce-content-body");
-        editor.clear();
-        editor.val(inputText);
-        editor.shouldHave(exactText(inputText));
     }
 
     @Test
